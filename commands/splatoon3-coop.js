d9bcc0540d35d78954b9ef6f990fd85515da8bb2
isak <t-yuta08780126@outlook.jp>
2022-11-07 22:07:03 +0900

    現在と次のサーモンランの情報を取得可能に

--- commands/splatoon3-coop.js
+++ commands/splatoon3-coop.js
@@ -1,44 +1,59 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { EmbedBuilder } = require("discord.js");
+//const { EmbedBuilder } = require("discord.js");
 //const buttonPages = require("../functions/pagination");
 const fs = require("fs");
 
+const splaApi = "https://spla3.yuu26.com/api/coop-grouping-regular/";
+
+const coopImgs = JSON.parse(fs.readFileSync("././resources/splatoon3-coopImages.json", "utf8")).coopImgs;
+
 module.exports = {
-    data: new SlashCommandBuilder().setName("coop").setDescription("Answer Splatoon3 coop information"),
+    data: new SlashCommandBuilder()
+        .setName("coop")
+        .setDescription("Answer Splatoon3 coop information")
+        .addStringOption((option) =>
+            option
+                .setName("time")
+                .setDescription("What time frame do you want the information in? ( now / next / schedule )")
+                .addChoices({ name: "now", value: "now" }, { name: "next", value: "next" }, { name: "schedule", value: "schedule" })
+                .setRequired(true),
+        ),
 
     run: async ({ interaction }) => {
-        const coopInfo = await (await fetch("https://spla3.yuu26.com/api/coop-grouping-regular/now")).json();
-        const coopImgs = JSON.parse(fs.readFileSync("././resources/splatoon3-coopImgs.json", "utf8")).coopImgs;
+        const time = interaction.options.getString("time", true);
+        // データを取得
+        let coopSchedule;
+        try {
+            coopSchedule = await (await fetch(`${splaApi}${time}`)).json();
+        } catch (e) {
+            console.log(e);
+            return interaction.reply({ content: "Error: Could not fetch data", ephemeral: true });
+        }
 
-        const startDate = coopInfo.results[0].start_time;
-        const endDate = coopInfo.results[0].end_time;
-        const weapons = coopInfo.results[0].weapons;
-
-        const description = `${startDate.slice(5, 7)}/${startDate.slice(8, 10)} ${startDate.slice(11, 13)}:00 ~ ${endDate.slice(5, 7)}/${endDate.slice(8, 10)} ${endDate.slice(11, 13)}:00`;
-
-        const img = coopImgs[coopInfo.results[0].stage.id];
-
-        const fields = [
-            {
-                name: "ブキ",
-                value: `${weapons[0].name}\n${weapons[1].name}\n${weapons[2].name}\n${weapons[3].name}`,
-                inline: true,
-            },
-            {
-                name: "ステージ",
-                value: coopInfo.results[0].stage.name,
-                inline: true,
-            },
-        ];
-        const embed = new EmbedBuilder()
-            .setColor("#FFAA00")
-            .setAuthor({
-                name: "サーモンラン",
-                iconURL: "https://i.imgur.com/QthCIvF.png",
-            })
-            .setDescription(description)
-            .addFields(fields)
-            .setImage(img);
-        return await interaction.editReply({ embeds: [embed] });
+        if (time == "now" || time == "next") {
+            const result = coopSchedule.results[0];
+            const startDate = result.start_time;
+            const endDate = result.end_time;
+            const coopDate = `${startDate.slice(5, 7)}/${startDate.slice(8, 10)} ${startDate.slice(11, 13)}:00 ~ ${endDate.slice(5, 7)}/${endDate.slice(8, 10)} ${endDate.slice(11, 13)}:00 (${time})`;
+            const weapons = result.weapons;
+            const embed = {
+                color: 0xffaa00,
+                author: {
+                    name: "サーモンラン",
+                    icon_url: "https://i.imgur.com/QthCIvF.png",
+                },
+                fields: [
+                    { name: "期間", value: coopDate, inline: false },
+                    { name: "ブキ", value: `${weapons[0].name}\n${weapons[1].name}\n${weapons[2].name}\n${weapons[3].name}`, inline: true },
+                    { name: "ステージ", value: `${result.stage.name}`, inline: true },
+                ],
+                image: { url: coopImgs[result.stage.id] },
+            };
+            return await interaction.editReply({ embeds: [embed] });
+        } else if (time == "schedule") {
+            return;
+        } else {
+            return;
+        }
     },
 };
