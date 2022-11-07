9a82d0121d8771f18c5c38cac08b87db864048c9
isak <t-yuta08780126@outlook.jp>
2022-11-07 22:18:16 +0900

    スケジュールに対応

--- commands/splatoon3-coop.js
+++ commands/splatoon3-coop.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 //const { EmbedBuilder } = require("discord.js");
-//const buttonPages = require("../functions/pagination");
+const buttonPages = require("../functions/pagination");
 const fs = require("fs");
 
 const splaApi = "https://spla3.yuu26.com/api/coop-grouping-regular/";
@@ -51,9 +51,32 @@ module.exports = {
             };
             return await interaction.editReply({ embeds: [embed] });
         } else if (time == "schedule") {
-            return;
+            const pages = [];
+            coopSchedule.results.forEach((result, index) => {
+                const startDate = result.start_time;
+                const endDate = result.end_time;
+                const coopDate = `${startDate.slice(5, 7)}/${startDate.slice(8, 10)} ${startDate.slice(11, 13)}:00 ~ ${endDate.slice(5, 7)}/${endDate.slice(8, 10)} ${endDate.slice(11, 13)}:00`;
+                const weapons = result.weapons;
+                const embed = {
+                    color: 0xffaa00,
+                    author: {
+                        name: "サーモンラン",
+                        icon_url: "https://i.imgur.com/QthCIvF.png",
+                    },
+                    fields: [
+                        { name: "期間", value: coopDate, inline: false },
+                        { name: "ブキ", value: `${weapons[0].name}\n${weapons[1].name}\n${weapons[2].name}\n${weapons[3].name}`, inline: true },
+                        { name: "ステージ", value: `${result.stage.name}`, inline: true },
+                    ],
+                    image: { url: coopImgs[result.stage.id] },
+                    footer: { text: `Page ${index + 1}/${coopSchedule.results.length}` },
+                };
+                pages.push(embed);
+            });
+            return buttonPages(interaction, pages);
         } else {
-            return;
+            const embed = { title: "Error" };
+            return await interaction.editReply({ embeds: [embed] });
         }
     },
 };
