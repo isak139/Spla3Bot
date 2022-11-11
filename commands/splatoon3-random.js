6935adcdb56e2e7ef07b893f8ea9403a3dce82d7
isak <t-yuta08780126@outlook.jp>
2022-11-12 00:02:46 +0900

    ランダムに二つ名を表示する機能を追加

--- commands/splatoon3-random.js
+++ commands/splatoon3-random.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const weaponInfo = JSON.parse(fs.readFileSync("././resources/splatoon3-weaponInfo.json", "utf8"));
 const stageInfo = JSON.parse(fs.readFileSync("././resources/splatoon3-stageInfo.json", "utf8"));
 const ruleInfo = JSON.parse(fs.readFileSync("././resources/splatoon3-ruleInfo.json", "utf8"));
+const titleInfo = JSON.parse(fs.readFileSync("././resources/splatoon3-titleInfo.json", "utf8"));
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,8 +13,8 @@ module.exports = {
         .addStringOption((option) =>
             option
                 .setName("query")
-                .setDescription("Which information do you want? ( weapon / stage / rule )")
-                .addChoices({ name: "weapon", value: "weapon" }, { name: "stage", value: "stage" }, { name: "rule", value: "rule" })
+                .setDescription("Which information do you want? ( weapon / stage / rule / title)")
+                .addChoices({ name: "weapon", value: "weapon" }, { name: "stage", value: "stage" }, { name: "rule", value: "rule" }, { name: "title", value: "title" })
                 .setRequired(true),
         ),
 
@@ -60,6 +61,17 @@ module.exports = {
                 },
                 title: rule[0],
                 image: { url: rule[1].image },
+            };
+            return interaction.editReply({ embeds: [embed] });
+        } else if (query == "title") {
+            const first = titleInfo.first[Math.floor(Math.random() * titleInfo.first.length)];
+            const second = titleInfo.second[Math.floor(Math.random() * titleInfo.second.length)];
+            const embed = {
+                author: {
+                    name: "ランダム二つ名",
+                    icon_url: "https://i.imgur.com/LjHn7tr.png",
+                },
+                title: `\`${first}\` \`${second}\``,
             };
             return interaction.editReply({ embeds: [embed] });
         } else {
