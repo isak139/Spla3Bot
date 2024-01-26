e38abe32a19118cde2158d41bcc9bfbde48b6d25
isak <t-yuta08780126@outlook.jp>
2024-01-26 22:16:49 +0900

    deferReplyしないように

--- index.js
+++ index.js
@@ -9,26 +9,18 @@ const CLIENT_ID = process.env.CLIENT_ID;
 //const GUILD_ID = process.env.GUILD_ID;
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers
-  ]
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers],
 });
 
 client.slashcommands = new Collection();
 
 let commands = [];
 
-const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.slashcommands[command.data.name] = command;
-  commands.push(command.data.toJSON());
+    const command = require(`./commands/${file}`);
+    client.slashcommands[command.data.name] = command;
+    commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: "9" }).setToken(TOKEN);
@@ -45,33 +37,32 @@ console.log("Deploying slash commands");
         }
     }) */
 // load slashcommands globally
-rest
-  .put(Routes.applicationCommands(CLIENT_ID), { body: commands })
-  .then(() => {
-    console.log("Successfully loaded global commands");
-  })
-  .catch((err) => {
-    if (err) {
-      console.log(err);
-      process.exit(1);
-    }
-  });
+rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands })
+    .then(() => {
+        console.log("Successfully loaded global commands");
+    })
+    .catch((err) => {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+    });
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`);
+    console.log(`Logged in as ${client.user.tag}`);
 });
 client.on("interactionCreate", (interaction) => {
-  async function handleCommand() {
-    if (!interaction.isCommand()) {
-      return;
+    async function handleCommand() {
+        if (!interaction.isCommand()) {
+            return;
+        }
+        const slashcmd = client.slashcommands[interaction.commandName];
+        if (!slashcmd) {
+            interaction.reply("Not a valid slash command");
+        }
+        //await interaction.deferReply();
+        await slashcmd.run({ client, interaction });
     }
-    const slashcmd = client.slashcommands[interaction.commandName];
-    if (!slashcmd) {
-      interaction.reply("Not a valid slash command");
-    }
-    await interaction.deferReply();
-    await slashcmd.run({ client, interaction });
-  }
-  handleCommand();
+    handleCommand();
 });
 client.login(TOKEN);
