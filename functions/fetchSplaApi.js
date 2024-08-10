ef01b143e35c8a067c9deba3f62f294ab58eddc4
isak <t-yuta08780126@outlook.jp>
2024-08-10 12:50:04 +0900

    フェス期間中にオープンマッチ，チャレンジマッチの情報を取得しようとするとBOTがクラッシュする不具合を修正．

--- functions/fetchSplaApi.js
+++ functions/fetchSplaApi.js
@@ -1,16 +1,18 @@
 const getSchedule = async (url, interaction) => {
     try {
         const res = await fetch(url);
-        if (res.ok) {
-            return await res.json();
-        } else {
-            const embed = {
-                color: 0xff0000,
-                description: "Error: Could not fetch data",
-            };
-            await interaction.reply({ embeds: [embed] });
-            return;
+        const json = await res.json();
+        if (res.ok && "results" in json) {
+            if (json.results.length > 0) {
+                return json;
+            }
         }
+        const embed = {
+            color: 0xff0000,
+            description: "Error: Could not fetch data",
+        };
+        await interaction.reply({ embeds: [embed] });
+        return;
     } catch (e) {
         console.log(e);
         const embed = {
